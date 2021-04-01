b45f7a997b0c524f3308543a597f6874c8240c42
thePalindra <palindra99@gmail.com>
2021-04-01 17:55:11 +0100

    1

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from flask import Flask
-from flask import request
-from flask import render_template
-import json
 import csv
+import json
+
+import pandas as pd
+from flask_paginate import Pagination, get_page_parameter
+from flask import Flask
+from flask import render_template
+from flask import request
+
+#Paginação para o número de vestidos
+ROWS_PER_PAGE = 10
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def index():
@@ -46,8 +51,25 @@ def getPhoto(name):
                     print(str(row[0].split(" ; ")[1]))
                     return(str(row[0].split(" ; ")[1]))
 
+@app.route('/getShop')
+def colors():
+    # Configurar paginação
+    search = False
+    q = request.args.get('q')
+    if q:
+        search = True
 
-
+    page = request.args.get(get_page_parameter(), 1, type=int)
+    """with open("./cap.dress.val copy.json") as json_file:
+        val = ast.literal_eval(json_file)
+        val1 = json.loads(json_file.read())
+        val2 = val1['target'][0]['candidate'][0]['captions']
+        table = pd.DataFrame(val1, columns=["target", "candidate", "captions"])"""
+    data = get_json()
+    print(data)
+    pagination = Pagination(page=page, total=len(data), search = search)
+    print(pagination)
+    return render_template('dresses.html', dresses = data, pagination=pagination)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
