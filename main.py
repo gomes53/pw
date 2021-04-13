270d26708923d294a589d5418ea278de388c070e
afonsorn <afonsornascimento99@gmail.com>
2021-04-13 13:29:05 +0100

    post files in folder to elastic search

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask import request
 from flask import render_template
 import json
 import csv
+import requests
 
 app = Flask(__name__)
 
@@ -26,27 +27,38 @@ def details():
 # def get_json():
 #     with open("./cap.dress.val.json") as json_file:
 #         return json_file.read()
-@app.route("/capDressVal")
-def get_json():
-    with open("./cap.dress.val.json") as json_file:
-        return json_file.read()
+# @app.route("/capDressVal")
+# def get_json():
+#     with open("./cap.dress.val.json") as json_file:
+#         return json_file.read()
 
 
 @app.route("/test")
 def test():
-    data = get_json()
-    return render_template('test.html', data = json.dumps(data))
+    return render_template('test.html')
 
-@app.route("/getPhoto/<name>")
-def getPhoto(name):
-    with open('data.csv', 'r') as file:
-            reader = csv.reader(file)
-            for row in reader:
-                if (row[0].split(" ; ")[0] == str(name)):
-                    print(str(row[0].split(" ; ")[1]))
-                    return(str(row[0].split(" ; ")[1]))
+# @app.route("/getPhoto/<name>")
+# def getPhoto(name):
+#     with open('data.csv', 'r') as file:
+#             reader = csv.reader(file)
+#             for row in reader:
+#                 if (row[0].split(" ; ")[0] == str(name)):
+#                     print(str(row[0].split(" ; ")[1]))
+#                     return(str(row[0].split(" ; ")[1]))
 
+@app.route("/products/<offset>")
+def getProductsOffset(offset):
+    results = {"products":[]}
+    off = int(offset)
+    headers = {'Content-type': 'application/json'}
+    for i in range((off-1)*10, off*10):
+        r = requests.get("http://localhost:9200/test/_doc/"+str(i), headers = headers)
+        # print(r.text)
+        results["products"].append(r.json())
+    return results
 
+# @app.route("/products/<filter>")
+# def getProductsFilter(filter):
 
 
 if __name__ == "__main__":
