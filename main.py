856f5ccb3ee4a8801edd38f9bae2b1488e18fa59
afonsorn <afonsornascimento99@gmail.com>
2021-04-28 22:43:50 +0100

    Added refine function

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from flask import request
 from flask import render_template
 import json
 import csv
+import sys
+import re
+import operator
 import requests
 from elasticsearch import Elasticsearch
 
@@ -22,31 +25,9 @@ def shop(offset, type):
 def details():
     return render_template('/product_details.html')
 
-# def get_json():
-#     with open("cap.dress.val.json") as json_file:
-#         return json.load(json_file)
-
-# def get_json():
-#     with open("./cap.dress.val.json") as json_file:
-#         return json_file.read()
-# @app.route("/capDressVal")
-# def get_json():
-#     with open("./cap.dress.val.json") as json_file:
-#         return json_file.read()
-
-
 @app.route("/test")
 def test():
     return render_template('test.html')
-
-# @app.route("/getPhoto/<name>")
-# def getPhoto(name):
-#     with open('data.csv', 'r') as file:
-#             reader = csv.reader(file)
-#             for row in reader:
-#                 if (row[0].split(" ; ")[0] == str(name)):
-#                     print(str(row[0].split(" ; ")[1]))
-#                     return(str(row[0].split(" ; ")[1]))
 
 @app.route("/products/<type>/<offset>")
 def getProductsOffset(offset,type):
@@ -64,33 +45,35 @@ def refineSearch(currentIndex,filter):
     headers = {'Content-type': 'application/json'}
     taxList = []
     ids = []
-    id1 = request.args.get('f1')
-    id2 = request.args.get('f2')
-    id3 = request.args.get('f3')
-    ids.append(id1)
-    ids.append(id2)
-    ids.append(id3)
-
-
+    for i in range(1,len(request.args)):
+        ids.append(request.args.get("f"+str(i)))
     
+    unwanted_chars = ".,-&"
+    wordfreq = {}
+
     for i in range(3):
         id = ids[i]
         req = requests.get('http://localhost:9200/'+currentIndex+'/_doc/'+id, headers = headers)
         js = json.loads(req.text)
         taxonomy = js["_source"]["taxonomy"]
-        taxList.append(taxonomy)
+        words = re.findall(r"[\w']+", taxonomy)
+        taxList.append(words)
+        for raw_word in words:
+            word = raw_word.strip(unwanted_chars)
+            if word not in wordfreq:
+                wordfreq[word] = 0 
+            wordfreq[word] += 1
 
-    for t in taxList:
-        print (t)
-    return getProductsFilter(currentIndex, filter, "red", 0, 10000)
-
-
+    orderedDict = dict(sorted(wordfreq.items(), key=lambda item: item[1]))
+    queryWords = []
+    for i in range(1,3):
+        queryWords.append(list(orderedDict.items())[-i][0])
+    
+    return getProductsFilter(currentIndex, filter, " ".join(queryWords), 0, 10000)
 
 #example: filter/dresses/taxonomy/red/0/1000
 @app.route("/filter/<currentIndex>/<filter>/<text>/<minPrice>/<maxPrice>")
 def getProductsFilter(currentIndex, filter, text, minPrice, maxPrice):
-    # text = request.args.get("text")
-    # filter = request.args.get("filter")
     results = {"products": []}
 
     if (text == "empty"):
@@ -128,23 +111,12 @@ def getProductsFilter(currentIndex, filter, text, minPrice, maxPrice):
             }
         }
     
-
-
-
     res = es.search(index= currentIndex, body=body)
 
     for i in res["hits"]["hits"]:
         results["products"].append(i)
 
-    #print (len(results["products"]))
-
     return jsonify(results)
-
-
-
-
-
-
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
